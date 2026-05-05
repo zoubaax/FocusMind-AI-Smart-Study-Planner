c19ca9f790207bf1fa9a3511df1d569712989388
zoubaa mohammed <zoubaax@gmail.com>
2026-05-05 21:25:29 +0100

    feat: initialize backend project structure with Maven Wrapper and base configuration

---
+++ backend/src/main/java/ma/zoubaa/smartstudyplanner/SmartStudyPlannerApplication.java
@@ -0,0 +1,13 @@
+package ma.zoubaa.smartstudyplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartStudyPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartStudyPlannerApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ma/zoubaa/smartstudyplanner/SmartStudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.zoubaa.smartstudyplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartStudyPlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
